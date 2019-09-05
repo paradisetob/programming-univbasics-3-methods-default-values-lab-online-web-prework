97984b4f0ed0e1e92477b59995d2d83681a09892
tracey brimage <onelovenilynn@gmail.com>
2019-09-05 15:07:33 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,4 @@
-# Your code here
+def meal_choice (veg1= peas, veg2=corn, protein ="meat")
+  puts "A plate of #{protein}" with #(veg1) and #(veg2)."
+  return A plate of #(protein) with #(veg1) adn #(veg2).
+end
